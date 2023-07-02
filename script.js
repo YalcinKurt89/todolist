363c42c4ac827323d0a83a80d09818b32c8dbcc1
Yalcin Kurt <yalcin.kurt89@gmail.com>
2023-07-03 00:41:00 +0200

    added files for html, css and javascript

---
+++ script.js
@@ -0,0 +1,41 @@
+function addTask() {
+    var input = document.getElementById("taskInput");
+    var task = input.value;
+  
+    if (task === "") {
+      alert("Please enter a task.");
+      return;
+    }
+  
+    var list = document.getElementById("taskList");
+    var li = document.createElement("li");
+    var completeButton = document.createElement("button");
+    var taskText = document.createElement("span");
+  
+    completeButton.className = "complete-button";
+    completeButton.onclick = function() {
+      toggleCompleted(taskText);
+      toggleCompleted(completeButton);
+    };
+  
+    taskText.appendChild(document.createTextNode(task));
+  
+    li.appendChild(completeButton);
+    li.appendChild(taskText);
+    list.appendChild(li);
+  
+    input.value = "";
+  }
+  
+  function toggleCompleted(taskText) {
+    taskText.classList.toggle("completed");
+  }
+
+
+  function handleKeyPress(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      var addButton = document.querySelector(".add-button");
+      addButton.click();
+    }
+  }
