39c22544da3c13397329d3be5eefdda4c03d27e2
Yalcin Kurt <yalcin.kurt89@gmail.com>
2024-03-01 21:24:47 +0100

    some changes

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 function addTask() {
     var input = document.getElementById("taskInput");
     var task = input.value;
-  
+
     if (task === "") {
       alert("Please enter a task.");
       return;
     }
-  
 
     var list = document.getElementById("taskList");
     var li = document.createElement("li");
     var completeButton = document.createElement("button");
     var taskText = document.createElement("span");
-  
 
     completeButton.className = "complete-button";
     completeButton.onclick = function() {
       toggleCompleted(taskText);
       toggleCompleted(completeButton);
     };
-  
 
     taskText.appendChild(document.createTextNode(task));
-  
 
     li.appendChild(completeButton);
     li.appendChild(taskText);
     list.appendChild(li);
-  
-    
+
     input.value = "";
-  }
+}
   
-
-
-  function toggleCompleted(taskText) {
+function toggleCompleted(taskText) {
     taskText.classList.toggle("completed");
-  }
+}
 
-
-
-  function handleKeyPress(event) {
+function handleKeyPress(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
       var addButton = document.querySelector(".add-button");
       addButton.click();
     }
-  }
+}
